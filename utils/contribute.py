3d3ab9c0e8153bfffdfe59e98a1cb669c2a2aecf
VannaM <vmavromatis18@gmail.com>
2016-11-02 20:40:59 -0400

    added files, debugging stuff

--- utils/contribute.py
+++ utils/contribute.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request, session, url_for
 import sqlite3
-app = Flask(__name__)
+#set proper template folder path
+app = Flask(__name__, template_folder = '../templates')
 
 @app.route('/contribute/<int:storyid>')
-def contribute(storyid):    
+def contribute(storyid):
     if 'user' not in session:
         #redirect(url_for('login'))
+        return render_template('loginTemplate.html', status = "")
     else:
         f = 'storymaker.db'
         db = sqlite3.connect(f)
-        c = db.cursor()        
-        q = "select user, sid from posts where sid == " + storyid
+        c = db.cursor()
+        q = "select user, sid from posts where sid == " + str(storyid)
         sel = c.execute(q)
         if session['user'] in sel:
             #redirect(url_for('viewstory'))
+            return render_template('viewStory.html', text = 'nothing here yet...')
         else:
             return render_template('contributeForm.html')
-        
-        
-        
+
+#for debugging purposes only
+#app.debug = True
+#app.run()
