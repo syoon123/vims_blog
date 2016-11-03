31a0890bdb0655f296c9eec6a5af74e2ad19ec98
VannaM <vmavromatis18@gmail.com>
2016-11-03 15:03:53 -0400

    minor debugging

--- app.py
+++ app.py
@@ -7,33 +7,33 @@ f="storymaker.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-#Initializing Tables: should be executed once (?)
-q = "CREATE TABLE user (user TEXT, pass TEXT)"
-c.execute(q)
+#table already created, debug w/out
 
-q = "CREATE TABLE posts (user TEXT, pid INTEGER, sid INTEGER, content TEXT)"
-c.execute(q)
-
-#test data
-q = "INSERT INTO user VALUES(\'mcVans\', \'vanna\')"
-c.execute(q)
-q = "INSERT INTO user VALUES(\'mcKissy\', \'issac\')"
-c.execute(q)
-q = "INSERT INTO user VALUES(\'mcCow\', \'michael\')"
-c.execute(q)
-q = "INSERT INTO user VALUES(\'mcYoonibrow\', \'sarah\')"
-c.execute(q)
-
-q = "INSERT INTO posts VALUES(\'mcVans\', 0,0,\'hello\')"
-c.execute(q)
-q = "INSERT INTO posts VALUES(\'mcKissy\', 1,0,\'my\')"
-c.execute(q)
-q = "INSERT INTO posts VALUES(\'mcCow\', 1,1,\'name\')"
-c.execute(q)
-q = "INSERT INTO posts VALUES(\'mcYoonibrow\', 2,0,\'is\')"
-c.execute(q)
-
-
+# #Initializing Tables: should be executed once (?)
+# q = "CREATE TABLE user (user TEXT, pass TEXT)"
+# c.execute(q)
+#
+# q = "CREATE TABLE posts (user TEXT, pid INTEGER, sid INTEGER, content TEXT)"
+# c.execute(q)
+#
+# #test data
+# q = "INSERT INTO user VALUES(\'mcVans\', \'vanna\')"
+# c.execute(q)
+# q = "INSERT INTO user VALUES(\'mcKissy\', \'issac\')"
+# c.execute(q)
+# q = "INSERT INTO user VALUES(\'mcCow\', \'michael\')"
+# c.execute(q)
+# q = "INSERT INTO user VALUES(\'mcYoonibrow\', \'sarah\')"
+# c.execute(q)
+#
+# q = "INSERT INTO posts VALUES(\'mcVans\', 0,0,\'hello\')"
+# c.execute(q)
+# q = "INSERT INTO posts VALUES(\'mcKissy\', 1,0,\'my\')"
+# c.execute(q)
+# q = "INSERT INTO posts VALUES(\'mcCow\', 1,1,\'name\')"
+# c.execute(q)
+# q = "INSERT INTO posts VALUES(\'mcYoonibrow\', 2,0,\'is\')"
+# c.execute(q)
 
 app = Flask(__name__)
 app.secret_key = '<j\x9ch\x80+\x0b\xd2\xb6\n\xf7\x9dj\xb8\x0fmrO\xce\xcd\x19\xd49\xe5S\x1f^\x8d\xb8"\x89Z'
@@ -60,7 +60,7 @@ def home():
 
 @app.route("/login/")
 def login():
-    if "user" in session:  
+    if "user" in session:
         return redirect(url_for("/"))
     return render_template("loginTemplate.html", status = "")
 
@@ -71,19 +71,19 @@ def authentication():
         register_message = register(request.form["user"],request.form["pass"])
         return render_template("loginTemplate.html", status = register_message)
     if request.form["enter"] == "Login":
-        session["user"] = request.form["user"] 
+        session["user"] = request.form["user"]
         #login_message = checkLogin(request.form["user"],request.form["pass"])
-        
+
         ##if correct login -> homepage
         return render_template("homepageTemplate.html")
 
         ##if incorrect login -> /login/   status = loginMsg
 
-@app.route("/logout/")    
+@app.route("/logout/")
 def logout():
     session.pop("user")
     return redirect(url_for("login"))
-        
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
