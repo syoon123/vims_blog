dcf68346b0c4f177f16e985ba05804f83cb57d0e
Issac Kim <ikim1@stuy.edu>
2016-11-04 14:52:26 -0400

    start find numb posts func

--- utils/newStory.py
+++ utils/newStory.py
@@ -7,21 +7,30 @@ import sqlite3
 #db.commit()
 #db.close()
 
-
+def getID():
+    f = "test.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    cmd = "SELECT * FROM posts WHERE post_id = 0;"
+    stories = c.execute(cmd)
+    db.close()
+    return len(stories)
+    
 def submit(post):
     f = "test.db"
-
+    story_id = getID()
     db = sqlite3.connect(f)
     c = db.cursor()
     if len(post) > 1000:
         return "post is too long, please resubmit"
     else:
         text = '''"''' + post + '''"'''
-        q = "INSERT INTO posts VALUES(%s, %i, %i, %s)" %('''"someone"''',0,0,text)
+        q = "INSERT INTO posts VALUES(%s, %i, %i, %s)" %('''"someone"''',story_id,0,text)
         c.execute(q)
         db.commit()
         db.close()
         return "post succesfully inputted"
 
+print(getID())
    
 
