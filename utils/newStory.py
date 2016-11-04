4ea09f49842484bc53fe12fa726ac3417b34abb7
Issac Kim <ikim1@stuy.edu>
2016-11-04 14:46:54 -0400

    debug database problem

--- utils/newStory.py
+++ utils/newStory.py
@@ -1,5 +1,11 @@
 import sqlite3
-
+#f = "test.db"
+#db = sqlite3.connect(f)
+#c = db.cursor()
+#q = "CREATE TABLE posts (user TEXT, post_id INTEGER, story_id INTEGER, post TEXT)"
+#c.execute(q)
+#db.commit()
+#db.close()
 
 
 def submit(post):
@@ -10,9 +16,12 @@ def submit(post):
     if len(post) > 1000:
         return "post is too long, please resubmit"
     else:
-        q = "INSERT INTO posts VALUES(%s, %i, %i, %s)" %("someone",1,1,post)
+        text = '''"''' + post + '''"'''
+        q = "INSERT INTO posts VALUES(%s, %i, %i, %s)" %('''"someone"''',0,0,text)
         c.execute(q)
+        db.commit()
+        db.close()
+        return "post succesfully inputted"
 
-    db.commit()
-    db.close()
+   
 
