5daa7ece2be7fb911864d28fea7f849db862b867
VannaM <vmavromatis18@gmail.com>
2016-11-06 19:06:47 -0500

    modified homepage

--- app.py
+++ app.py
@@ -4,51 +4,25 @@ from utils import contribute, newStory
 
 f="storymaker.db"
 
-#db = sqlite3.connect(f) #open if f exists, otherwise create
-#c = db.cursor()    #facilitate db ops
-#
-##table already created, debug w/out
-#
-# #Initializing Tables: should be executed once (?)
-#q = "CREATE TABLE user (user TEXT, pass TEXT)"
-#c.execute(q)
-#
-#q = "CREATE TABLE posts (user TEXT, pid INTEGER, sid INTEGER, content TEXT)"
-#c.execute(q)
-#
-# #test data
-#q = "INSERT INTO user VALUES(\'mcVans\', \'vanna\')"
-#c.execute(q)
-#q = "INSERT INTO user VALUES(\'mcKissy\', \'issac\')"
-#c.execute(q)
-#q = "INSERT INTO user VALUES(\'mcCow\', \'michael\')"
-#c.execute(q)
-#q = "INSERT INTO user VALUES(\'mcYoonibrow\', \'sarah\')"
-#c.execute(q)
-#
-#q = "INSERT INTO posts VALUES(\'mcVans\', 0,0,\'hello\')"
-#c.execute(q)
-#q = "INSERT INTO posts VALUES(\'mcKissy\', 1,0,\'my\')"
-#c.execute(q)
-#q = "INSERT INTO posts VALUES(\'mcCow\', 1,1,\'name\')"
-#c.execute(q)
-#q = "INSERT INTO posts VALUES(\'mcYoonibrow\', 2,0,\'is\')"
-#c.execute(q)
-
 app = Flask(__name__)
 app.secret_key = '<j\x9ch\x80+\x0b\xd2\xb6\n\xf7\x9dj\xb8\x0fmrO\xce\xcd\x19\xd49\xe5S\x1f^\x8d\xb8"\x89Z'
 
 def register(username, password):
     db = sqlite3.connect(f)
     c = db.cursor()
-    query = "SELECT user FROM users";
+    query = "SELECT user FROM users"
     dbUsers = c.execute(query)
+
     for entry in dbUsers:
-        if (entry[0] == username): return "You are already registered."  #check if username is taken
+        if (entry[0] == username):
+            return "You are already registered."  #check if username is taken
+
     hashedPass = hashlib.sha1(password).hexdigest()
     insertQuery = "INSERT INTO users VALUES(\'%s\',\'%s\')"%(username,hashedPass)
+
     c = db.cursor()
     c.execute(insertQuery)
+
     db.commit()
     db.close()
     return "You are now successfully registered."
@@ -57,7 +31,7 @@ def checkLogin(username,password):
     hashedPass = hashlib.sha1(password).hexdigest()
     db = sqlite3.connect(f)
     c = db.cursor()
-    query = "SELECT * FROM users";
+    query = "SELECT * FROM users"
     dbUserPass = c.execute(query)
     for entry in dbUserPass:
         if (entry[0] == username):
@@ -74,17 +48,27 @@ def home():
     #get links to stories
     linkStr = ""
     allStoryStr = ""
+
     db = sqlite3.connect(f)
     c = db.cursor()
-    d = db.cursor()
-    query = "SELECT sid FROM posts WHERE user = %s"%(session["user"])
-    storyHistory = c.execute(query)
+    query = "select sid from posts where user = " + session['user']
+    try:
+        storyHistory = c.execute(query)
+    #if you have no stories?
+    except:
+        storyHistory = ""
+
     for entry in storyHistory:
         linkStr+= "<a href = '%s'></a><br>"%(entry[0])
+
     query = "SELECT sid FROM posts"
-    allStories = d.execute(query)
+    allStories = c.execute(query)
+    allStoryStr += "<center>"
+
     for entry in allStories:
-        allStoryStr+= "a href = '%s'></a><br>"%(entry[0])
+        allStoryStr+= "<a href = '%s'>%s</a><br>"%(entry[0],entry[0])
+
+    allStoryStr += "</center>"
 
     return render_template("homepageTemplate.html", storyHistory = linkStr, allStories = allStoryStr)
 
@@ -99,11 +83,11 @@ def login():
         return render_template("loginTemplate.html", status = register_message)
     if request.form["enter"] == "Login":
         login_message = checkLogin(request.form["user"],request.form["pass"])
-        if (login_message == ""): 
+        if (login_message == ""):
             session["user"] = request.form["user"]
             return redirect(url_for("home"))
         return render_template("loginTemplate.html", status = login_message)
-    
+
 @app.route("/logout/")
 def logout():
     session.pop("user")
@@ -112,7 +96,7 @@ def logout():
 @app.route("/newstory/")
 def post():
     return render_template("make_post.html")
-    
+
 @app.route("/postResult/", methods = ['POST'])
 def result():
     r = request.form
@@ -120,7 +104,7 @@ def result():
     user = session['user']
     newStory.submit(post, user)
     return redirect(url_for('home'))
-        
+
 @app.route('/contribute/<int:storyid>')
 def add(storyid):
     return contribute.addToStory(storyid)
